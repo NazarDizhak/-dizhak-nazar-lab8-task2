85a64a7e1ad3d68b6a7711daf4b2214f36da2da1
NazarDizhak <dizhak.pn@ucu.edu.ua>
2023-11-07 16:09:38 +0200

    Create dizhak-nazar-lab8-task2.py

---
+++ dizhak-nazar-lab8-task2.py
@@ -0,0 +1,23 @@
+'''
+puzzle game
+https://github.com/NazarDizhak/dizhak-nazar-lab8-task2.git
+'''
+def rows_check(board:str) -> bool:
+    '''checking rows for duplicate elements'''
+    pass
+
+def columns_check(board:str) -> bool:
+    '''checking columns for duplicate elements'''
+    pass
+
+def colors_check(board:str) -> bool:
+    '''checking colors for duplicate elements'''
+    pass
+
+def validate_board(board:str) -> bool:
+    '''taking all() from previous functions to validate a board'''
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
