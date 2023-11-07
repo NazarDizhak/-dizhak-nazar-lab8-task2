aa3fab6c311b20b0e828d153bbcb9cda68e14b1c
NazarDizhak <dizhak.pn@ucu.edu.ua>
2023-11-07 16:19:52 +0200

    added doctests

--- dizhak-nazar-lab8-task2.py
+++ dizhak-nazar-lab8-task2.py
@@ -3,7 +3,20 @@ puzzle game
 https://github.com/NazarDizhak/dizhak-nazar-lab8-task2.git
 '''
 def rows_check(board:str) -> bool:
-    '''checking rows for duplicate elements'''
+    '''checking rows for duplicate elements
+    >>> board = ["**** ****",\
+                 "***1 ****",\
+                 "**  3****",\
+                 "* 4 1****",\
+                 "     9 5 ",\
+                 " 6  83  *",\
+                 "3   1  **",\
+                 "  8  2***",\
+                 "  2  ****"]
+
+    >>> rows_check(board)
+    True
+    '''
     res = True
     for row in board:
         temp = []
@@ -15,7 +28,20 @@ def rows_check(board:str) -> bool:
     return res
 
 def columns_check(board:str) -> bool:
-    '''checking columns for duplicate elements'''
+    '''checking columns for duplicate elements
+    >>> board = ["**** ****",\
+                 "***1 ****",\
+                 "**  3****",\
+                 "* 4 1****",\
+                 "     9 5 ",\
+                 " 6  83  *",\
+                 "3   1  **",\
+                 "  8  2***",\
+                 "  2  ****"]
+
+    >>> columns_check(board)
+    False
+    '''
     res = True
     for col in range(9):
         temp = []
@@ -28,7 +54,20 @@ def columns_check(board:str) -> bool:
     return res
 
 def colors_check(board:str) -> bool:
-    '''checking colors for duplicate elements'''
+    '''checking colors for duplicate elements
+    >>> board = ["**** ****",\
+                 "***1 ****",\
+                 "**  3****",\
+                 "* 4 1****",\
+                 "     9 5 ",\
+                 " 6  83  *",\
+                 "3   1  **",\
+                 "  8  2***",\
+                 "  2  ****"]
+
+    >>> colors_check(board)
+    True
+    '''
     colors = []
     for i in range(5):
         temp_1 = [(board[8-i][i+j]) for j in range(4)]
